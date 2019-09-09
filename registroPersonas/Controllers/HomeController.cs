c6da7ddfbf15760c0f7085da22d2b98ae933a8c5
Jasson Gonzalez <jasson0802@gmail.com>
2019-09-09 16:51:46 -0600

    First commit

---
+++ registroPersonas/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace registroPersonas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+            //_log.Debug("Home page hit!");
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            //ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            //ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Person()
+        {
+            //ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
